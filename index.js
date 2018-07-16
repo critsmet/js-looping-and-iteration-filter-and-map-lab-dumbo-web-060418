948b57083e142ee9694a34455fe09036fb5a87a5
Christopher Metzger <chrismetzgr@gmail.com>
2018-07-16 14:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ function driversWithRevenueOver(driver, revenue){
 function driverNamesWithRevnueOver(driver, revenue){
   let driverNames = driversWithRevenueOver(driver, revenue)
   driverNames.map ( function (driver){
-    return driver.name }
-  })
+    return driver.name });
 }
