45430c50ecfd9100946742124d64dc5f2eeb9b7c
Christopher Metzger <chrismetzgr@gmail.com>
2018-07-16 14:30:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here:
 
 function driversWithRevenueOver(driver, revenue){
-  return driver.filter( function (driver){
-    return   
+  return driver.map( function (driver){
+    return
   })
 }
